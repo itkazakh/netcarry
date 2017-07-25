e76b2a16fe22b6e14376beae8384f4bf09dc3e73
yinyayun <yinyayun1314@gmail.com>
2017-07-25 23:09:04 +0800

    多页面抓取，剔除抓取到的重复内容

--- netcarry-business/src/main/java/org/yinyayun/netcarry/biz/youku/YoukuPageParser.java
+++ netcarry-business/src/main/java/org/yinyayun/netcarry/biz/youku/YoukuPageParser.java
@@ -7,9 +7,12 @@ package org.yinyayun.netcarry.biz.youku;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import org.apache.commons.io.FileUtils;
 import org.jsoup.Jsoup;
@@ -25,6 +28,8 @@ import org.yinyayun.netcarry.core.parser.FetchParser;
  * @author yinyayun
  */
 public class YoukuPageParser extends FetchParser<Map<String, String>> {
+    private Set<String> links = Collections.synchronizedSet(new HashSet<String>());
+
     public YoukuPageParser(FetchCollector<Map<String, String>> collector) {
         super(collector);
     }
@@ -38,9 +43,12 @@ public class YoukuPageParser extends FetchParser<Map<String, String>> {
             Elements aElements = element.getElementsByTag("a");
             String title = aElements.get(0).attr("title");
             String href = aElements.get(0).attr("href");
-            map.put("title", title);
-            map.put("href", href);
-            values.add(map);
+            if (!links.contains(href)) {
+                map.put("title", title);
+                map.put("href", href);
+                values.add(map);
+                links.add(href);
+            }
         }
         return values;
     }
